e5a0a8d750321b4049bd32bbb01c6c1a0d0e2dfd
Li Shen <lililogan@gmail.com>
2023-07-26 12:17:59 -0700

    button

--- components/PathSync.js
+++ components/PathSync.js
@@ -12,10 +12,33 @@ export default function PathSync() {
 
             <div className=''>
                 <div className='flex lg:flex-row flex-col justify-center items-center lg:py-12'>
-                    <div className='flex text-center lg:text-start items-center lg:text-[60px] text-[25px] font-CG font-bold pb-4 text-primary-blue'>
+                
+                    <div className='flex flex-col justify-start lg:items-start items-center'>
+
+                         <div className='flex text-center lg:text-start items-center lg:text-[60px] text-[25px] font-CG font-bold  text-primary-blue'>
                         Project Management & Analysis for Dual Organization Collaboration 
+                        </div>
+
+                            
+                        <div className='py-4 flex justify-center'>
+                            <a
+                                className="text-sm font-semibold opacity-80 text-primary-blue font-Mont text-center px-6 py-2 shadow-sm hover:shadow-none rounded-full border-[1px] border-primary-blue"
+                                href="https://pathsync.vercel.app"
+                                target="_blank"
+                                rel="noreferrer noopener">
+                                <div className='flex gap-2 align-center items-center'>
+                                <div>
+                                Try BETA Ver. 1.1
+                                </div>
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+                                </svg>
+                                </div>
+                            </a>
+                        </div>
                     </div>
-                    <div class="flex align-center lg:justify-center lg:w-screen justify-center max-w-[300px] lg:max-w-[700px]">
+                    
+                    <div class="flex align-center lg:justify-center lg:w-screen justify-center max-w-[300px] lg:max-w-[700px] py-4">
                         <video src= "/pathsync/pathSync.mp4"
                             autoplay="{true}" 
                             loop="{true}"
@@ -23,7 +46,8 @@ export default function PathSync() {
                         </video>
                     </div>
                 </div>
-                <div className='lg:text-md text-sm pt-4 lg:pt-0'>
+
+                <div className='lg:text-md text-sm pt-4 lg:pt-0 font-bold opacity-40'>
                     Personal Project 
                 </div>
             </div>
